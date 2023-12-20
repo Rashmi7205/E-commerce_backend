dcaa6d4fbc1766121710b0e4d13bf1be6833156a
Rashmi7205 <rashmiranjanbehera8260@gmail.com>
2023-12-20 12:54:34 +0530

    cart Schema

---
+++ models/cart.schema.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+
+const cartItemSchema = new mongoose.Schema({
+    product:{type:mongoose.Schema.Types.ObjectId,ref:'Product',required:true},
+    quantity:{type:Number,requird:true},
+},{timestamps:true});
+
+
+const cartSchema = new mongoose.Schema({
+    user:{type:mongoose.Schema.Types.ObjectId,ref:'User',required:true},
+    items:[cartItemSchema],
+},{timestamps:true});
+
+export default mongoose.model("Cart",cartSchema);
+
